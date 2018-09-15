7983a927d3377ebdddef82c803b8e7ec18e9b893
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-09-14 21:14:28 -0400

    Creation unit tests pour OrienteurTransaction.

--- millegrilles/processus/OrienteurTransaction.py
+++ millegrilles/processus/OrienteurTransaction.py
@@ -31,3 +31,36 @@ class OrienteurTransaction(BaseCallback):
         self.dict_libelle = {
             "senseur.lecture": "MGPProcessus.Senseur.ConsignerLecture"
         }
+
+    '''
+    :param message: Evenement d'initialisation de processus recu de la Q (format dictionnaire).
+    
+    :raises ErreurInitialisationProcessus: le processus est inconnu
+    '''
+    def orienter_message(self, dictionnaire_evenement):
+        libelle = dictionnaire_evenement.get('libelle')
+
+        if libelle is None:
+            raise ErreurInitialisationProcessus(dictionnaire_evenement)
+
+        processus_correspondant = self.dict_libelle.get(libelle)
+        if processus_correspondant is None:
+            raise ErreurInitialisationProcessus(dictionnaire_evenement)
+
+        return processus_correspondant
+
+'''
+Exception lancee lorsque le processus ne peut pas etre initialise (erreur fatale).
+'''
+
+
+class ErreurInitialisationProcessus(Exception):
+
+    def __init__(self, evenement):
+        super().__init__(self)
+        self._evenement = evenement
+
+    @property
+    def evenement(self):
+        return self._evenement
+
---
+++ test/processus/TestOrienteurTransaction.py
@@ -0,0 +1,51 @@
+import unittest
+from millegrilles.processus.OrienteurTransaction import OrienteurTransaction
+from millegrilles.processus.OrienteurTransaction import ErreurInitialisationProcessus
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_something(self):
+        self.assertEqual(True, True)
+
+
+class OrienteurTransactionTest(unittest.TestCase):
+
+    def setUp(self):
+        self._orienteur = OrienteurTransaction()
+        self._message = None
+
+    def test_chargement_liste_processus(self):
+        self._orienteur.charger_liste_processus()
+        self.assertGreater(len(self._orienteur.dict_libelle), 0, "Le dictionnaire de libelles est vide")
+
+    def test_orienter_message_vide(self):
+        try:
+            self._orienteur.orienter_message({})
+            self.fail("ErreurInitialisationProcessus aurait du etre lance")
+        except ErreurInitialisationProcessus:
+            pass
+        except Exception:
+            self.fail("ErreurInitialisationProcessus aurait du etre lance")
+
+    def test_orienter_message_processus_connu(self):
+        self._orienteur.dict_libelle = {"senseur.lecture": "MGPProcessus.Senseur.ConsignerLecture"}
+        processus = self._orienteur.orienter_message({"libelle":"senseur.lecture"})
+        self.assertEqual(processus, "MGPProcessus.Senseur.ConsignerLecture")
+
+    def test_orienter_message_processus_inconnu(self):
+        #self._orienteur.dict_libelle = {"senseur.lecture": "MGPProcessus.Senseur.ConsignerLecture"}
+        try:
+            processus = self._orienteur.orienter_message({"libelle":"senseur.lecture"})
+            self.fail("ErreurInitialisationProcessus aurait du etre lance")
+        except ErreurInitialisationProcessus:
+            pass
+
+if __name__ == '__main__':
+    unittest.main()
