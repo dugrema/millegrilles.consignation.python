2ca5181ba5ebec38710f9fd54e6db84b051aed81
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-09-18 20:17:30 -0400

    Modification ProcessusTest.
    Voir si les parametres se comportent correctement, enchainement de plusieurs etapes.

--- millegrilles/processus/ProcessusTest.py
+++ millegrilles/processus/ProcessusTest.py
@@ -1,5 +1,8 @@
 # Module de Processus a utiliser pour tester MilleGrilles
 from millegrilles.processus.MGProcessus import MGProcessus
+from millegrilles import Constantes
+import time
+
 
 class TestOrienteur(MGProcessus):
 
@@ -10,8 +13,29 @@ class TestOrienteur(MGProcessus):
     def initiale(self):
         print("Etape initiale executee avec evenement: %s" % self._evenement)
         self._initiale_executee = True
-        self.set_etape_suivante() # Set etape suivante, par defaut utilise finale
+        self.set_etape_suivante(TestOrienteur.etape1.__name__)
 
         # Implicitement l'etape devrait etre enregistree dans le document de processus
         return {'texte': 'La methode initiale de ProcessusTest est completee'}
 
+    def etape1(self):
+        self.set_etape_suivante(TestOrienteur.etape2.__name__)
+
+        # Implicitement l'etape devrait etre enregistree dans le document de processus
+        return {
+            'nombre': 123,
+            'dict_donnees': {
+                'greetings': 'allo',
+                'calcul': 123.948
+            }
+        }
+
+    def etape2(self):
+        self.set_etape_suivante() # Etape finale par defaut
+
+        # Implicitement l'etape devrait etre enregistree dans le document de processus
+        return {
+            'epoch': int(time.time()),
+            'nombre': 234,
+            'etape-courante-evenement': self._document_processus[Constantes.PROCESSUS_DOCUMENT_LIBELLE_ETAPESUIVANTE]
+        }
