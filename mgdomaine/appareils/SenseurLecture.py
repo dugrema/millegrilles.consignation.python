271c5ca8c7c961461160871b16e9ea44c60e8d7b
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-13 15:09:18 -0500

    Deplacement du processus dans Appareils.py vers repertoire domaine.
    Il va rester a corriger le chargement des processus pour supporter l'utilisation de package global.

--- mgdomaine/appareils/SenseurLecture.py
+++ mgdomaine/appareils/SenseurLecture.py
@@ -173,3 +173,35 @@ class GenerateurPagesNoeudsStatistiquesDernierMois(GenerateurRapportParAggregati
         chemin_evenement = evenement.get(Constantes.DOCUMENT_INFODOC_CHEMIN)
         # return self._source[Constantes.DOCUMENT_INFODOC_CHEMIN] == chemin_evenement
         return False # Le trigger n'existe pas encore (cedule)
+
+
+'''
+Processus pour importer une lecture dans MilleGrilles.
+'''
+class ProcessusSenseurConserverLecture(MGProcessusTransaction):
+
+    def __init__(self, controleur, evenement):
+        super().__init__(controleur, evenement)
+
+    def dao_helper(self):
+        helper = AppareilInformationDocumentHelper(self._controleur.document_dao(), self._controleur.message_dao())
+        return helper
+
+    def initiale(self):
+        helper = self.dao_helper()
+        transaction = self.charger_transaction()
+
+        lecture = transaction['charge-utile']
+
+        # Verifier que le noeud est fourni - sinon on utilie le nom de domaine d'origine de la transaction
+        if lecture.get('noeud') is None:
+            # On va utiliser l'information de source du message pour donner un nom implicite au noeud.
+            # Normalement ca va etre le nom de domaine: mathieu@cuisine.maple.mdugre,info, on garde cuisine.maple.mdugre.info
+            source = transaction[Constantes.TRANSACTION_MESSAGE_LIBELLE_INFO_TRANSACTION][Constantes.TRANSACTION_MESSAGE_LIBELLE_SOURCE_SYSTEME]
+            source = source.split('@')[1]
+            lecture['noeud'] = source
+
+        helper.sauvegarder_senseur_lecture(lecture)
+
+        # Terminer
+        self.set_etape_suivante()
