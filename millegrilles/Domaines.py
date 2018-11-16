7559cf815625c93f06e088b8e2287bce3436f634
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-16 15:11:15 -0500

    Preparation du gestionnaire de senseurs passifs, avec Q.

--- millegrilles/Domaines.py
+++ millegrilles/Domaines.py
@@ -1,7 +1,8 @@
 # Module avec utilitaires generiques pour mgdomaines
 from millegrilles.dao.Configuration import TransactionConfiguration
-from millegrilles.dao.MessageDAO import PikaDAO
+from millegrilles.dao.MessageDAO import PikaDAO, JSONHelper
 from millegrilles.dao.DocumentDAO import MongoDAO
+from millegrilles.processus.MGProcessus import MGPProcessusDemarreur
 
 
 # Le gestionnaire de domaine est une superclasse qui definit le cycle de vie d'un domaine.
@@ -11,6 +12,8 @@ class GestionnaireDomaine:
         self.configuration = None
         self.message_dao = None
         self.document_dao = None
+        self.demarreur_processus = None
+        self.json_helper = JSONHelper()
 
     ''' L'initialisation connecte RabbitMQ, MongoDB, lance la configuration '''
     def initialiser(self):
@@ -18,6 +21,11 @@ class GestionnaireDomaine:
         self.configuration.loadEnvironment()
         self.message_dao = PikaDAO(self.configuration)
         self.document_dao = MongoDAO(self.configuration)
+
+        self.document_dao.connecter()
+        self.message_dao.connecter()
+
+        self.demarreur_processus = MGPProcessusDemarreur(self.message_dao, self.document_dao)
 
     ''' Configure les comptes, queues/bindings (RabbitMQ), bases de donnees (MongoDB), etc. '''
     def configurer(self):
@@ -28,8 +36,15 @@ class GestionnaireDomaine:
         pass
 
     ''' Demarre le traitement des messages pour le domaine '''
-    def demarrer_traitement_messages(self):
-        pass
+    def demarrer_traitement_messages_blocking(self, queue_name):
+        self.message_dao.channel.basic_consume(self.traiter_transaction, queue=queue_name, no_ack=False)
+        try:
+            self.message_dao.channel.start_consuming()
+        except OSError as oserr:
+            print("erreur start_consuming, probablement du a la fermeture de la queue: %s" % oserr)
+
+    def traiter_transaction(self, ch, method, properties, body):
+        raise NotImplementedError("N'est pas implemente - doit etre definit dans la sous-classe")
 
     ''' Arrete le traitement des messages pour le domaine '''
     def arreter_traitement_messages(self):
@@ -37,4 +52,9 @@ class GestionnaireDomaine:
 
     ''' Permet de deconnecter les DAOs, fermer le gestionnaire. '''
     def deconnecter(self):
-        pass
+        self.document_dao.deconnecter()
+        self.message_dao.deconnecter()
+
+    def demarrer_processus(self, processus, parametres):
+        self.demarreur_processus.demarrer_processus(processus, parametres)
+
