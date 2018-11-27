ddd0e224cb9e400953e461c6591b6ed3a6cb9669
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-27 08:37:45 -0500

    Ajout .gitignore, update version dans setup.py to 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrilles.consignation.python',
-    version='0.3',
+    version='0.4',
     packages=[
         'millegrilles',
         'millegrilles.dao',
