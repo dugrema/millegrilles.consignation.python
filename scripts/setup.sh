396d9e03097ddcce62c53e900a7323675909cd3f
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-12-04 14:08:50 -0500

    Copie des fichiers docker dans repertoire source.

---
+++ scripts/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Fichier de setup pour container Docker (doit etre execute dans le container via Dockerfile)
+
+GIT_NAME=MilleGrilles.consignation.python
+GIT_FOLDER=$SRC_FOLDER/$GIT_NAME
+
+echo "Installer dependances Python avec pip: fichier $GIT_FOLDER/requirements.txt"
+http_proxy=http://192.168.1.28:8000 pip install --no-cache-dir -r $GIT_FOLDER/requirements.txt
+
+echo Installer package MilleGrilles.consignation
+cd $GIT_FOLDER
+python3 setup.py install
+
+echo "Copier script demarrer dans $BUNDLE_FOLDER"
+mkdir -p $BUNDLE_FOLDER
+cp $GIT_FOLDER/demarrer*.py $BUNDLE_FOLDER
+
+cd $BUNDLE_FOLDER
+rm -rf $BUILD_FOLDER
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrilles.consignation.python',
-    version='0.6',
+    version='0.6.3',
     packages=[
         'millegrilles',
         'millegrilles.dao',
