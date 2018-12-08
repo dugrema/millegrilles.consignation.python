992232d9b31d512ea1fca0e052a41470f07bd5cb
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-12-08 12:26:30 -0500

    Update version a 0.8.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrilles.consignation.python',
-    version='0.8.1',
+    version='0.8.2',
     packages=[
         'millegrilles',
         'millegrilles.dao',
