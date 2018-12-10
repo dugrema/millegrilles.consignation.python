0ed35535ff89f20e4c3b7bc2e90fa76ce94e7ee2
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-12-10 18:16:30 -0500

    Combination des scripts setup.sh consignation et domaine.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,8 +5,20 @@
 GIT_NAME=MilleGrilles.consignation.python
 GIT_FOLDER=$SRC_FOLDER/$GIT_NAME
 
-echo "Installer dependances Python avec pip: fichier $GIT_FOLDER/requirements.txt"
-http_proxy=http://192.168.1.28:8000 pip install --no-cache-dir -r $GIT_FOLDER/requirements.txt
+REQ_FILE=$GIT_FOLDER/requirements.txt
+if [ ! -f $REQ_FILE ]; then
+    echo Fichier $REQ_FILE introuvable
+
+    echo Repertoire git: $GIT_FOLDER
+    echo Repertoire src: $SRC_FOLDER
+
+    ls -la $SRC_FOLDER
+    ls -la $GIT_FOLDER
+    exit 1
+fi
+
+echo "Installer dependances Python avec pip: fichier $REQ_FILE"
+http_proxy=http://192.168.1.28:8000 pip3 install --no-cache-dir -r $REQ_FILE
 
 echo Installer package MilleGrilles.consignation
 cd $GIT_FOLDER
@@ -15,6 +27,8 @@ python3 setup.py install
 echo "Copier script demarrer dans $BUNDLE_FOLDER"
 mkdir -p $BUNDLE_FOLDER
 cp $GIT_FOLDER/scripts/demarrer*.py $BUNDLE_FOLDER
+# Copier fichier de reference pour la configuration de tous les domaines
+cp $GIT_FOLDER/scripts/domaines.json $BUNDLE_FOLDER
 
 cd $BUNDLE_FOLDER
 rm -rf $BUILD_FOLDER
