bac3ddcc305493df07ccea73586ca93d75303cc9
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-02 17:14:28 -0500

    Update setup v1.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrilles.consignation.python',
-    version='1.2.0',
+    version='1.2.2',
     packages=[
         'millegrilles',
         'millegrilles.dao',
