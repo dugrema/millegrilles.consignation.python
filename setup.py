a53d7a66eb4f720e5fd54f891828621c34873646
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-14 18:51:25 -0500

    Build 1.3.5.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrilles.consignation.python',
-    version='1.3.4',
+    version='1.3.5',
     packages=[
         'millegrilles',
         'millegrilles.dao',
