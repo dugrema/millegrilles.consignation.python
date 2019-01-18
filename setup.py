bbb1ac05884280cb2239427c68b6368ef3c9d9b7
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-18 18:08:58 -0500

    Build 1.3.9. Fix bug avec contexte dans rapport SenseursPassifs.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrilles.consignation.python',
-    version='1.3.8',
+    version='1.3.9',
     packages=[
         'millegrilles',
         'millegrilles.dao',
