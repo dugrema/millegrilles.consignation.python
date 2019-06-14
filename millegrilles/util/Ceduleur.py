8728673e1c1fe36c01a2b1c52e773e9cc78200ec
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-06-14 09:42:35 -0400

    Ceduleur: ne charge plus document DAO.

--- millegrilles/util/Ceduleur.py
+++ millegrilles/util/Ceduleur.py
@@ -19,7 +19,7 @@ class CeduleurMilleGrilles(ModeleConfiguration):
         self._stop_event.set()
         self.logger = logging.getLogger('%s.CeduleurMilleGrilles' % __name__)
 
-    def initialiser(self, init_document=True, init_message=True, connecter=True):
+    def initialiser(self, init_document=False, init_message=True, connecter=True):
         super().initialiser(init_document, init_message, connecter)
 
     def configurer_parser(self):
