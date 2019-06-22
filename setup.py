fd4ea2c334fb3583a3d756d7006643916afb9367
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-06-22 08:09:10 -0400

    Branch 1.7.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrilles.consignation.python',
-    version='1.6.3',
+    version='1.7.0',
     packages=[
         'millegrilles',
         'millegrilles.dao',
