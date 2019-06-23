80ee9266c194ff34960bbabdc1afebe22e6d4d5a
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-06-22 20:34:04 -0400

    1.7.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrilles.consignation.python',
-    version='1.7.1',
+    version='1.7.2',
     packages=[
         'millegrilles',
         'millegrilles.dao',
