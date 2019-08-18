f6de2b9afc476698cd15ec119c436002e59b1a5e
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-08-18 08:31:32 -0400

    Utilisation fichier .txt pour transmettre version a docker.

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@ import subprocess
 
 
 def get_version():
-    with open('build.txt', "r") as buildno_file:
-        build_no = buildno_file.read()
+    try:
+        with open('version.txt', 'r') as version_file:
+            version = version_file.read()
+    except FileNotFoundError as e:
+        with open('build.txt', "r") as buildno_file:
+            build_no = buildno_file.read()
 
-    commande_git_version = ['git', 'name-rev', '--name-only', 'HEAD']
-    output_process = subprocess.run(commande_git_version, stdout=subprocess.PIPE)
-    version = output_process.stdout.decode('utf8').strip()
-    print("Version: %s.%s" % (version, build_no))
+        commande_git_version = ['git', 'name-rev', '--name-only', 'HEAD']
+        output_process = subprocess.run(commande_git_version, stdout=subprocess.PIPE)
+        version = output_process.stdout.decode('utf8').strip()
+        print("Version: %s.%s" % (version, build_no))
+
     return version
 
 
