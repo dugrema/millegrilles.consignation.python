2db1394f41bb9320cc0517f23e361b0494992fa6
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-09-28 16:13:14 +0000

    Reconfig maitredescles local, nouveau script pour secrets en dev.

---
+++ test/divers/prep_certsfolder.py
@@ -0,0 +1,9 @@
+import os
+
+folder = '/home/mathieu/mgdev/certs'
+
+for file in os.listdir(folder):
+    name_split = file.split('.')
+    name_short = '.'.join(name_split[1:-1])
+
+    os.symlink('%s/%s' % (folder, file), '%s/%s' % (folder, name_short))
