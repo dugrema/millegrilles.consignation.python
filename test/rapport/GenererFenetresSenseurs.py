eb7d7cc8c90aca1f662921c94d9b96cb093ed156
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-10-31 13:58:15 +0000

    Travail pour ajouter rapports/fenetres sur senseurs.

--- test/rapport/GenererFenetresSenseurs.py
+++ test/rapport/GenererFenetresSenseurs.py
@@ -1,10 +1,11 @@
 from millegrilles import Constantes
 from millegrilles.dao.Configuration import ContexteRessourcesMilleGrilles
 from millegrilles.domaines.SenseursPassifs import SenseursPassifsConstantes
+from millegrilles.dao.DocumentDAO import MongoJSONEncoder
 
 import logging
 import datetime
-
+import json
 
 class GenererRapportsFenetresSenseurs:
 
@@ -14,7 +15,7 @@ class GenererRapportsFenetresSenseurs:
         self._contexte = ContexteRessourcesMilleGrilles()
         self._contexte.initialiser(init_document=True, init_message=True)
 
-    def rapport_quotidien(self):
+    def rapport_senseurs(self, uuid_senseur: str = None, niveau_regroupement: str = 'hour'):
         collection_transactions = self._contexte.document_dao.get_collection(SenseursPassifsConstantes.COLLECTION_TRANSACTIONS_NOM)
 
         # LIBELLE_DOCUMENT_SENSEUR_RAPPORT_HORAIRE
@@ -29,31 +30,39 @@ class GenererRapportsFenetresSenseurs:
                 '$lt': temps_fin_rapport.timestamp(),
             }
         }
+        if uuid_senseur is not None:
+            filtre_rapport[SenseursPassifsConstantes.TRANSACTION_ID_SENSEUR] = uuid_senseur
 
         regroupement_periode = {
             'year': {'$year': '$_evenements._estampille'},
             'month': {'$month': '$_evenements._estampille'},
-            'day': {'$dayOfMonth': '$_evenements._estampille'}
         }
+        if niveau_regroupement == 'day':
+            regroupement_periode['day'] = {'$dayOfMonth': '$_evenements._estampille'}
+        elif niveau_regroupement == 'hour':
+            regroupement_periode['day'] = {'$dayOfMonth': '$_evenements._estampille'}
+            regroupement_periode['hour'] = {'$hour': '$_evenements._estampille'}
+
+        regroupement_elem_numeriques = [
+            'temperature', 'humidite', 'pression', 'millivolt', 'reserve'
+        ]
+        accumulateurs = ['max', 'min', 'avg']
+
         regroupement = {
             '_id': {
-                'senseur': '$uuid_senseur',
-                'periode': {
-                    '$dateFromParts': regroupement_periode
-                },
+                'uuid_senseur': '$uuid_senseur',
                 'appareil_type': '$senseurs.type',
                 'appareil_adresse': '$senseurs.adresse',
+                'timestamp': {
+                    '$dateFromParts': regroupement_periode
+                },
             },
-            'temperature_max': {'$max': '$senseurs.temperature'},
-            'temperature_min': {'$min': '$senseurs.temperature'},
-            'temperature_avg': {'$avg': '$senseurs.temperature'},
-            'humidite_max': {'$max': '$senseurs.humidite'},
-            'humidite_min': {'$min': '$senseurs.humidite'},
-            'humidite_avg': {'$avg': '$senseurs.humidite'},
-            'pression_max': {'$max': '$senseurs.pression'},
-            'pression_min': {'$min': '$senseurs.pression'},
-            'pression_avg': {'$avg': '$senseurs.pression'},
         }
+
+        for elem_regroupement in regroupement_elem_numeriques:
+            for accumulateur in accumulateurs:
+                key = '%s_%s' % (elem_regroupement, accumulateur)
+                regroupement[key] = {'$%s' % accumulateur: '$senseurs.%s' % elem_regroupement}
 
         # resultat = collection_transactions.find(filter=filtre_rapport)
 
@@ -65,13 +74,62 @@ class GenererRapportsFenetresSenseurs:
 
         resultat = collection_transactions.aggregate(operation)
         self._logger.info("Resultats")
-        for transaction in resultat:
-            self._logger.info(str(transaction))
+        # Key=uuid_senseur, Value=[{appareil_type, appareil_adresse, timestamp, accums...}, ...]
+        resultats_par_senseur = dict()
+        for ligne_rapport in resultat:
+            # self._logger.info(str(ligne_rapport))
+            resultats_appareil = resultats_par_senseur.get(ligne_rapport['_id']['uuid_senseur'])
+            if resultats_appareil is None:
+                resultats_appareil = dict()
+                resultats_par_senseur[ligne_rapport['_id']['uuid_senseur']] = resultats_appareil
 
+            # Reorganiser valeurs pour insertion dans document de rapport
+            cle_appareil = ligne_rapport['_id']['appareil_type']
+            if cle_appareil == 'onewire/temperature':
+                adresse = ligne_rapport['_id'].get('appareil_adresse')
+                cle_appareil = '1W%s' % adresse
+
+            liste_valeurs = resultats_appareil.get(cle_appareil)
+            if liste_valeurs is None:
+                liste_valeurs = list()
+                resultats_appareil[cle_appareil] = liste_valeurs
+
+            ligne_formattee = dict()
+            liste_valeurs.append(ligne_formattee)
+
+            ligne_formattee['timestamp'] = ligne_rapport['_id']['timestamp']
+
+            for elem_regroupement in regroupement_elem_numeriques:
+                for accumulateur in accumulateurs:
+                    key = '%s_%s' % (elem_regroupement, accumulateur)
+                    valeur = ligne_rapport[key]
+                    if valeur is not None:
+                        ligne_formattee[key] = valeur
+
+        self._logger.info(json.dumps(resultats_par_senseur, indent=2, cls=MongoJSONEncoder))
+        return resultats_par_senseur
+
+    def inserer_resultats_rapport(self, resultats: dict, nombre_resultats_limite: int = 175):
+
+        for uuid_senseur, appareils in resultats.items():
+            self._logger.info("Inserer resultats dans document %s" % uuid_senseur)
+            push_operation = dict()
+            for appareil, valeurs in appareils.items():
+                # Ajouter les valeurs en ordre croissant de timestamp.
+                # Garder les "nombre_resultats_limite" plus recents (~1 semaine)
+                push_operation['appareils.%s' % appareil] = {
+                    '$each': valeurs,
+                    '$sort': {SenseursPassifsConstantes.TRANSACTION_DATE_LECTURE: 1},
+                    '$slice': -nombre_resultats_limite,
+                }
+
+            self._logger.info('Operation push: %s' % str(push_operation))
 
 def main():
     test = GenererRapportsFenetresSenseurs()
-    test.rapport_quotidien()
+    # test.rapport_senseurs(uuid_senseur='f14951f2f43211e99259b827eb53ee51', niveau_regroupement='hour')
+    resultats = test.rapport_senseurs()
+    test.inserer_resultats_rapport(resultats)
 
 
 if __name__ == '__main__':
