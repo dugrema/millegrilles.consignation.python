b32b0ea28dc1a809f6da483398b3464c69a5b91f
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-01-28 03:07:19 +0000

    Convert bytes pour dhcp

--- test/divers/random_string.py
+++ test/divers/random_string.py
@@ -1,17 +1,17 @@
 import os
 import binascii
+import struct
 
-test = os.urandom(4)
-
+test = os.urandom(3)
 print(str(test))
-
 test = binascii.hexlify(test)
-
 test_string = test.decode('utf8')
-
 print(test_string)
-
 test_back = binascii.unhexlify(test_string.encode('utf8'))
-
 print(test_back)
+
+result = bytes(b'\x00\x00') + test_back + bytes(3)
+print(binascii.hexlify(result))
+
+print("Resultat unpack : %s " % hex(struct.unpack('Q', result)[0]))
 
