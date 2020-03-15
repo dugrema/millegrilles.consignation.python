d72b494f18f7a365f1018e706829cbb57d97bdcd
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-03-15 16:58:29 +0000

    Restauration d'un fichier, mapping evenements en datetime.

--- test/prototype/backup_transactions.py
+++ test/prototype/backup_transactions.py
@@ -54,8 +54,10 @@ class MessagesSample(BaseCallback):
         self.__logger.debug(str(body))
 
     def executer(self):
-        # self.backup_transactions_senseurspassifs()
-        self.backup_horaire_domaine_senseurpassifs()
+        # self.backup_domaine_senseurpassifs()
+        self.restore_horaire_domaine_senseurspassifs()
+
+        self.event_recu.set()  # Termine
 
     def backup_transactions_senseurspassifs_testinit(self):
         coltrans = self.contexte.document_dao.get_collection(SenseursPassifsConstantes.COLLECTION_TRANSACTIONS_NOM)
@@ -70,15 +72,61 @@ class MessagesSample(BaseCallback):
                 # json_transaction = self.__json_helper.dict_vers_json(transaction)
                 # self.__logger.debug("Transaction %s" % json_transaction)
 
-    def backup_horaire_domaine_senseurpassifs(self):
+    def backup_domaine_senseurpassifs(self):
         nom_collection_mongo = SenseursPassifsConstantes.COLLECTION_TRANSACTIONS_NOM
         idmg = 'bKKwtXC68HR4TPDzet6zLVq2wPJfc9RiiYLuva'
-        heure = datetime.datetime(year=2020, month=1, day=6, hour=19)
-        self.backup_horaire_domaine(nom_collection_mongo, idmg, heure)
+        # heure = datetime.datetime(year=2020, month=1, day=6, hour=19)
+        heure_courante = datetime.datetime.utcnow()
+        heure = datetime.datetime(year=heure_courante.year, month=heure_courante.month, day=heure_courante.day, hour=heure_courante.hour, tzinfo=datetime.timezone.utc)
+        heure = heure - datetime.timedelta(hours=1)
+        self.__logger.debug("Faire backup horaire de %s" % str(heure))
+        self.backup_domaine(nom_collection_mongo, idmg, heure)
 
-    def backup_horaire_domaine(self, nom_collection_mongo, idmg, heure):
-        delta = datetime.timedelta(hours=1)
-        heure_fin = heure + delta
+    def backup_domaine(self, nom_collection_mongo, idmg, heure):
+        # Verifier s'il y a des transactions qui n'ont pas ete traitees avant la periode actuelle
+        filtre_verif_transactions_anterieures = {
+            '_evenements.transaction_complete': True,
+            '_evenements.%s.transaction_traitee' % idmg: {'$lt': heure}
+        }
+        regroupement_periode = {
+            'year': {'$year': '$_evenements.%s.transaction_traitee' % idmg},
+            'month': {'$month': '$_evenements.%s.transaction_traitee' % idmg},
+            'day': {'$dayOfMonth': '$_evenements.%s.transaction_traitee' % idmg},
+            'hour': {'$hour': '$_evenements.%s.transaction_traitee' % idmg},
+        }
+        regroupement = {
+            '_id': {
+                'timestamp': {
+                    '$dateFromParts': regroupement_periode
+                },
+            },
+        }
+        sort = {'_id': 1}
+        operation = [
+            {'$match': filtre_verif_transactions_anterieures},
+            {'$group': regroupement},
+            {'$sort': sort},
+        ]
+        hint = {
+            '_evenements.transaction_complete': 1,
+            '_evenements.bKKwtXC68HR4TPDzet6zLVq2wPJfc9RiiYLuva.transaction_traitee': 1
+        }
+        coltrans = self.contexte.document_dao.get_collection(nom_collection_mongo)
+
+        # projection = [
+        #     '_evenements.%s.transaction_traitee' % idmg
+        # ]
+        # for transanter in coltrans.find(filtre_verif_transactions_anterieures, projection=projection):
+        #     self.__logger.debug("Vieille transaction : %s" % str(transanter))
+
+        for transanter in coltrans.aggregate(operation, hint=hint):
+            self.__logger.debug("Vieille transaction : %s" % str(transanter))
+            heure_anterieure = transanter['_id']['timestamp']
+            self.backup_horaire_domaine(nom_collection_mongo, idmg, heure_anterieure)
+
+    def backup_horaire_domaine(self, nom_collection_mongo: str, idmg: str, heure: datetime):
+        heure_str = heure.strftime("%Y%m%d%H")
+        heure_fin = heure + datetime.timedelta(hours=1)
         self.__logger.debug("Backup collection %s entre %s et %s" % (nom_collection_mongo, heure, heure_fin))
 
         coltrans = self.contexte.document_dao.get_collection(nom_collection_mongo)
@@ -94,21 +142,45 @@ class MessagesSample(BaseCallback):
         ]
 
         curseur = coltrans.find(filtre, sort=sort)
-        with lzma.open('/tmp/senseurspassifs.json.xz', 'wt') as fichier:
+        with lzma.open('/tmp/mgbackup/senseurspassifs_%s.json.xz' % heure_str, 'wt') as fichier:
             for transaction in curseur:
                 json.dump(transaction, fichier, sort_keys=True, ensure_ascii=True, cls=BackupFormatEncoder)
 
                 # Une transaction par ligne
                 fichier.write('\n')
 
+    def restore_horaire_domaine_senseurspassifs(self):
+        nom_collection_mongo = SenseursPassifsConstantes.COLLECTION_TRANSACTIONS_NOM
+        idmg = 'bKKwtXC68HR4TPDzet6zLVq2wPJfc9RiiYLuva'
+        path_fichier = '/tmp/mgbackup/senseurspassifs_2020010619.json.xz'
+
+        self.restore_horaire_domaine(nom_collection_mongo, idmg, path_fichier)
+
+    def restore_horaire_domaine(self, nom_collection_mongo: str, idmg: str, path_fichier: str):
+        with lzma.open(path_fichier, 'rt') as fichier:
+            for line in fichier:
+                transaction = json.loads(line)
+
+                # Restaurer les dates dans l'element _evenements
+                evenements = transaction['_evenements']
+                evenements['_estampille'] = datetime.datetime.fromtimestamp(evenements['_estampille'] / 1000)
+
+                for idmg, events_par_mg in evenements.items():
+                    if not idmg.startswith('_') and isinstance(events_par_mg, dict):
+                        dates_corrigees = dict()
+                        for event_name, ts_int in events_par_mg.items():
+                            dates_corrigees[event_name] = datetime.datetime.fromtimestamp(ts_int / 1000)
+                        evenements[idmg] = dates_corrigees
+
+                self.__logger.debug("Transaction : %s" % str(transaction))
 
 # -------
 logging.basicConfig()
 sample = MessagesSample()
 
 # TEST
-sample.executer()
+# sample.executer()
 
 # FIN TEST
-# sample.event_recu.wait(10)
+sample.event_recu.wait(200)
 sample.deconnecter()
