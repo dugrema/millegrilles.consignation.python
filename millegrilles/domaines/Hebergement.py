ab6d297ff31b13e139f083235d3e74a6f403cdfc
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-04-13 14:43:51 +0000

    Travail sur hebergement. Ajout license GNU AGPLv3.

--- millegrilles/domaines/Hebergement.py
+++ millegrilles/domaines/Hebergement.py
@@ -4,7 +4,8 @@ import datetime
 
 from millegrilles import Constantes
 from millegrilles.Constantes import ConstantesHebergement
-from millegrilles.Domaines import GestionnaireDomaineStandard, TraitementMessageDomaineRequete
+from millegrilles.Domaines import GestionnaireDomaineStandard, TraitementMessageDomaineRequete, \
+    TraitementCommandesProtegees
 from millegrilles.MGProcessus import MGProcessusTransaction
 
 
@@ -12,6 +13,20 @@ class TraitementRequetesProtegees(TraitementMessageDomaineRequete):
 
     def traiter_requete(self, ch, method, properties, body, message_dict):
         super().traiter_requete(ch, method, properties, body, message_dict)
+
+
+class TraitementCommandesHebergementProtegees(TraitementCommandesProtegees):
+
+    def traiter_commande(self, enveloppe_certificat, ch, method, properties, body, message_dict):
+        routing_key = method.routing_key
+
+        resultat = None
+        if routing_key == 'commande.%s.%s' % (ConstantesHebergement.DOMAINE_NOM, ConstantesHebergement.COMMANDE_SIGNER_CLE_BACKUP):
+            resultat = self.gestionnaire.signer_cle_backup(properties, message_dict)
+        else:
+            resultat = super().traiter_commande(enveloppe_certificat, ch, method, properties, body, message_dict)
+
+        return resultat
 
 
 class GestionnaireHebergement(GestionnaireDomaineStandard):
@@ -53,9 +68,27 @@ class GestionnaireHebergement(GestionnaireDomaineStandard):
         return self.__handler_requetes_noeuds
 
     def identifier_processus(self, domaine_transaction):
-        if domaine_transaction == ConstantesHebergement.TRANSACTION_CATALOGUE_HORAIRE:
-            processus = "millegrilles_domaines_Backup:ProcessusAjouterCatalogueHoraire"
+        if domaine_transaction == ConstantesHebergement.TRANSACTION_XXX:
+            processus = "millegrilles_domaines_Hebergement:ProcessusXXX"
         else:
             processus = super().identifier_processus(domaine_transaction)
 
         return processus
+
+    # --- Operations du domaine ---
+
+    def creer_domaine_heberge(self) -> str:
+        """
+        Genere un nouveau domaine heberge.
+        :return: Le idmg du nouveau
+        """
+
+
+class ProcessusXXX(MGProcessusTransaction):
+
+    def __init__(self, controleur, evenement, transaction_mapper=None):
+        super().__init__(controleur, evenement, transaction_mapper)
+        self.__logger = logging.getLogger(__name__ + '.' + self.__class__.__name__)
+
+    def initiale(self):
+        transaction = self.charger_transaction()
