351f4bac1eb0ed98fc41602e23b85412d9b798d7
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-04-23 17:47:37 +0000

    Prep module hebergement transactions.

--- millegrilles/Hebergement.py
+++ millegrilles/Hebergement.py
@@ -1,16 +1,38 @@
-class Hebergement:
+import logging
+
+from millegrilles.util.UtilScriptLigneCommandeMessages import ModeleConfiguration
+
+
+class Hebergement(ModeleConfiguration):
 
     def __init__(self):
-        pass
+        super().__init__()
+        self.__millegrilles = None
+
+        self.__logging = logging.getLogger(__name__ + '.' + self.__class__.__name__)
+
+    def executer(self):
+        self.__logging.info("Demarrage hebergement")
+
+        self.__logging.info("Arret hebergement")
 
 
 class HebergementTransactions(Hebergement):
 
     def __init__(self):
         super().__init__()
+        self.__logging = logging.getLogger(__name__ + '.' + self.__class__.__name__)
 
 
 class HebergementDomaines(Hebergement):
 
     def __init__(self):
         super().__init__()
+        self.__logging = logging.getLogger(__name__ + '.' + self.__class__.__name__)
+
+
+class HebergementMaitreDesCles(Hebergement):
+
+    def __init__(self):
+        super().__init__()
+        self.__logging = logging.getLogger(__name__ + '.' + self.__class__.__name__)
