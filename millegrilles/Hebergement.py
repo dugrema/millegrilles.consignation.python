f22e6165688bdbb4a91124d10ff8ac0a26bd989a
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-04-23 21:32:55 +0000

    Demarrage module hebergement transactions

--- millegrilles/Hebergement.py
+++ millegrilles/Hebergement.py
@@ -1,5 +1,7 @@
 import logging
 
+from threading import Event
+
 from millegrilles.util.UtilScriptLigneCommandeMessages import ModeleConfiguration
 
 
@@ -8,11 +10,18 @@ class Hebergement(ModeleConfiguration):
     def __init__(self):
         super().__init__()
         self.__millegrilles = None
+        self.__fermeture_event = Event()
 
         self.__logging = logging.getLogger(__name__ + '.' + self.__class__.__name__)
 
+    def deconnecter(self):
+        self.__fermeture_event.set()
+
     def executer(self):
         self.__logging.info("Demarrage hebergement")
+
+        while not self.__fermeture_event.is_set():
+            self.__fermeture_event.wait(10)
 
         self.__logging.info("Arret hebergement")
 
