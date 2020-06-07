fe25027ebf6f7e4bf65bbc8f548061205397d3b3
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-06-07 12:29:16 +0000

    Utilisation path relatif avec symlink certs en dev

--- test/divers/prep_certsfolder.py
+++ test/divers/prep_certsfolder.py
@@ -2,11 +2,16 @@ import os
 
 folder = '/home/mathieu/mgdev/certs'
 
+print("Working directory : " + str(os.getcwd()))
+
 for file in os.listdir(folder):
     name_split = file.split('.')
     name_short = '.'.join(name_split[1:-1])
 
     try:
-        os.symlink('%s/%s' % (folder, file), '%s/%s' % (folder, name_short))
-    except:
-        pass
+        # os.symlink('%s/%s' % (folder, file), '%s/%s' % (folder, name_short))
+        path_dest = os.fspath(name_short)
+        print(str(path_dest))
+        os.symlink(file, path_dest)
+    except Exception as e:
+        print("Exception " + str(e))
