71fb541308aef00d1e9dc203063c097b4b4081d6
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-07-03 18:08:39 +0000

    Setup script installation postgres

--- test/apps/script.shared.postgres.installation.sh
+++ test/apps/script.shared.postgres.installation.sh
@@ -3,7 +3,16 @@
 EXIT_CODE=0
 
 echo "[OK] Demarrage script d'installation de shared.postgres"
+
+# Attente postgres
+sleep 5
+
+echo "Script Redmine"
 su -c "psql -f /tmp/apps/script.postgres.redmine.sql" postgres
+
+echo "Script Blynk"
+su -c "psql -f /tmp/apps/script.postgres.blynk.sql" postgres
+
 echo "[OK] Fin du script d'installation de postgres"
 
 echo "{\"exit\": $EXIT_CODE}"
