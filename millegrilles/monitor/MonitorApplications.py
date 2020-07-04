e5a7d4dcc18a22516af8473daa58a326dcb644a1
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-07-04 01:18:53 +0000

    Config et lecture scripts installation apps

--- millegrilles/monitor/MonitorApplications.py
+++ millegrilles/monitor/MonitorApplications.py
@@ -59,7 +59,7 @@ class GestionnaireApplications:
         nom_image_docker = commande.contenu['nom_application']
         # self.__logger.info("Telecharger image docker %s" % nom_image_docker)
 
-        self.__gestionnaire_images_applications.charger_versions()
+        self.__gestionnaire_images_applications.set_configuration(commande.contenu['configuration'])
 
         config_name = self.__service_monitor.idmg_tronque + '.app.' + nom_image_docker
         config_content = self.__gestionnaire_images_applications.config
@@ -127,9 +127,12 @@ class GestionnaireImagesApplications(GestionnaireImagesDocker):
 
         self._versions_images: dict = cast(dict, None)
 
-    def charger_versions(self):
-        with open('/home/mathieu/PycharmProjects/millegrilles.consignation.python/test/json/postgres.sharedapp.json', 'r') as fichier:
+    def set_configuration(self, fichier):
+        with open(fichier, 'r') as fichier:
             self._versions_images = json.load(fichier)
+
+    def charger_versions(self):
+        pass
 
     @property
     def config(self):
