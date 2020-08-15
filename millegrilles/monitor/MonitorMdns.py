e7f02891a1277a10ce11bf8882df4eaa3ce0d22f
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-08-15 16:57:45 +0000

    Cleanup service (mdns), pas utilise

--- millegrilles/monitor/MonitorMdns.py
+++ millegrilles/monitor/MonitorMdns.py
@@ -11,11 +11,7 @@ class MdnsGestionnaire:
 
     def __init__(self, monitor):
         self.__monitor = monitor
-        # self.__service = MdnsService(monitor)
         self.__browser = MdnsBrowser(monitor)
-
-    # def ajouter_service(self, nom_service: str, type_service: str, port: int, properties: dict = None):
-    #     self.__service.ajouter_service(nom_service, type_service, port, properties)
 
     def get_service(self, idmg: str, nom_service: str):
         service_info = None
@@ -31,81 +27,81 @@ class MdnsGestionnaire:
         self.__browser.fermer()
 
 
-class MdnsService:
-    """
-    Permet d'exposer des services via mdns. Supporte ipv4 et ipv6.
-    """
-
-    def __init__(self, monitor):
-        self.__logger = logging.getLogger(__name__ + '.' + self.__class__.__name__)
-        self.services_exposes = dict()
-        self.ipv4: Optional[str] = None
-        self.ipv6: Optional[str] = None
-        self.hostname: Optional[str] = None
-        self.zeroconf_ipv4: Optional[Zeroconf] = None
-        self.zeroconf_ipv6: Optional[Zeroconf] = None
-
-        self.monitor = monitor
-
-        self.__detecter_adresses()
-
-        self.desc = dict()
-        if self.monitor.idmg:
-            self.desc['idmg'] = self.monitor.idmg
-
-        if self.ipv4:
-            self.__logger.info("Initialisation de services zeroconf sur ipv4: %s" % self.ipv4)
-            self.zeroconf_ipv4 = Zeroconf(ip_version=IPVersion.V4Only)
-
-        if self.ipv6:
-            self.__logger.info("Initialisation de services zeroconf sur ipv6: %s" % self.ipv6)
-            self.zeroconf_ipv6 = Zeroconf(ip_version=IPVersion.V6Only)
-
-    def __detecter_adresses(self):
-        ips = get_local_ips()
-        self.ipv4 = ips.get('ipv4')
-        self.ipv6 = ips.get('ipv6')
-        self.hostname = socket.gethostname()
-
-    def ajouter_service(self, nom_service: str, type_service: str, port: int, properties: dict = None):
-        if self.ipv4:
-            service_ipv4 = self.__creer_service(socket.AF_INET, nom_service, type_service, port)
-            self.zeroconf_ipv4.register_service(service_ipv4)
-
-        if self.ipv6:
-            service_ipv6 = self.__creer_service(socket.AF_INET6, nom_service, type_service, port)
-            self.zeroconf_ipv6.register_service(service_ipv6)
-
-    def __creer_service(self, type_ip: int, nom_service: str, type_service: str, port: int, properties: dict = None):
-        if type_ip == socket.AF_INET:
-            adresse_ip = self.ipv4
-        elif type_ip == socket.AF_INET6:
-            adresse_ip = self.ipv6
-        else:
-            raise ValueError("Type ip invalide : %d" % type_ip)
-
-        if not properties:
-            properties = dict()
-        properties.update(self.desc)
-
-        info = ServiceInfo(
-            type_service,
-            nom_service + "." + type_service,
-            parsed_addresses=[adresse_ip],
-            port=port,
-            properties=properties,
-            # server=self.hostname,
-        )
-
-        return info
-
-    def fermer(self):
-        if self.zeroconf_ipv4:
-            self.zeroconf_ipv4.unregister_all_services()
-            self.zeroconf_ipv4.close()
-        if self.zeroconf_ipv6:
-            self.zeroconf_ipv6.unregister_all_services()
-            self.zeroconf_ipv6.close()
+# class MdnsService:
+#     """
+#     Permet d'exposer des services via mdns. Supporte ipv4 et ipv6.
+#     """
+#
+#     def __init__(self, monitor):
+#         self.__logger = logging.getLogger(__name__ + '.' + self.__class__.__name__)
+#         self.services_exposes = dict()
+#         self.ipv4: Optional[str] = None
+#         self.ipv6: Optional[str] = None
+#         self.hostname: Optional[str] = None
+#         self.zeroconf_ipv4: Optional[Zeroconf] = None
+#         self.zeroconf_ipv6: Optional[Zeroconf] = None
+#
+#         self.monitor = monitor
+#
+#         self.__detecter_adresses()
+#
+#         self.desc = dict()
+#         if self.monitor.idmg:
+#             self.desc['idmg'] = self.monitor.idmg
+#
+#         if self.ipv4:
+#             self.__logger.info("Initialisation de services zeroconf sur ipv4: %s" % self.ipv4)
+#             self.zeroconf_ipv4 = Zeroconf(ip_version=IPVersion.V4Only)
+#
+#         if self.ipv6:
+#             self.__logger.info("Initialisation de services zeroconf sur ipv6: %s" % self.ipv6)
+#             self.zeroconf_ipv6 = Zeroconf(ip_version=IPVersion.V6Only)
+#
+#     def __detecter_adresses(self):
+#         ips = get_local_ips()
+#         self.ipv4 = ips.get('ipv4')
+#         self.ipv6 = ips.get('ipv6')
+#         self.hostname = socket.gethostname()
+#
+#     def ajouter_service(self, nom_service: str, type_service: str, port: int, properties: dict = None):
+#         if self.ipv4:
+#             service_ipv4 = self.__creer_service(socket.AF_INET, nom_service, type_service, port)
+#             self.zeroconf_ipv4.register_service(service_ipv4)
+#
+#         if self.ipv6:
+#             service_ipv6 = self.__creer_service(socket.AF_INET6, nom_service, type_service, port)
+#             self.zeroconf_ipv6.register_service(service_ipv6)
+#
+#     def __creer_service(self, type_ip: int, nom_service: str, type_service: str, port: int, properties: dict = None):
+#         if type_ip == socket.AF_INET:
+#             adresse_ip = self.ipv4
+#         elif type_ip == socket.AF_INET6:
+#             adresse_ip = self.ipv6
+#         else:
+#             raise ValueError("Type ip invalide : %d" % type_ip)
+#
+#         if not properties:
+#             properties = dict()
+#         properties.update(self.desc)
+#
+#         info = ServiceInfo(
+#             type_service,
+#             nom_service + "." + type_service,
+#             parsed_addresses=[adresse_ip],
+#             port=port,
+#             properties=properties,
+#             # server=self.hostname,
+#         )
+#
+#         return info
+#
+#     def fermer(self):
+#         if self.zeroconf_ipv4:
+#             self.zeroconf_ipv4.unregister_all_services()
+#             self.zeroconf_ipv4.close()
+#         if self.zeroconf_ipv6:
+#             self.zeroconf_ipv6.unregister_all_services()
+#             self.zeroconf_ipv6.close()
 
 
 class MdnsBrowser:
