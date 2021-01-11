957d8e9487806e2c2f44825299762092f44203b0
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-01-11 21:33:08 +0000

    Test validateur de messages avec chargement cert par fingerprint

--- test/divers/TestValidateursMessages.py
+++ test/divers/TestValidateursMessages.py
@@ -1,18 +1,9 @@
-import datetime
-import pytz
 import logging
-import json
 
-from os import path
-from certvalidator.errors import PathValidationError
-from typing import Dict
-from threading import Event
 from cryptography.exceptions import InvalidSignature
 
 from millegrilles import Constantes
-from millegrilles.Constantes import ConstantesGenerateurCertificat
 from millegrilles.util.ValidateursMessages import ValidateurMessage
-from millegrilles.util.X509Certificate import RenouvelleurCertificat, EnveloppeCleCert
 from millegrilles.dao.Configuration import ContexteRessourcesMilleGrilles
 from millegrilles.SecuritePKI import HachageInvalide
 
@@ -22,36 +13,50 @@ class ValiderMessage:
     def __init__(self):
         self.__logger = logging.getLogger('__main__.' + self.__class__.__name__)
         self.contexte = None
+        self.validateur = None
 
     def initialiser_contexte(self):
         self.contexte = ContexteRessourcesMilleGrilles()
         self.contexte.initialiser()
 
-    def test_valider_message(self):
         self.__logger.debug("Preparation validateur message")
-        validateur = ValidateurMessage(self.contexte)
-        validateur.connecter()
+        self.validateur = ValidateurMessage(self.contexte)
+        self.validateur.connecter()
         self.__logger.debug("Validateur pret")
 
+    def test_valider_message(self):
         message_signe = self.contexte.generateur_transactions.preparer_enveloppe(
             {'test': True}, domaine='test', ajouter_certificats=True)
 
         # Tester message de base
-        enveloppe_certificat = validateur.verifier(message_signe, utiliser_date_message=True)
+        enveloppe_certificat = self.validateur.verifier(message_signe, utiliser_date_message=True)
         self.__logger.debug("Message valide, certificat %s" % enveloppe_certificat.fingerprint_sha256_b64)
 
+    def test_valider_message_chargercertificat(self):
+        message_signe = self.contexte.generateur_transactions.preparer_enveloppe({'test': True}, domaine='test')
+
+        # Tester message de base
+        enveloppe_certificat = self.validateur.verifier(message_signe, utiliser_date_message=True)
+        self.__logger.debug("Message valide, certificat %s" % enveloppe_certificat.fingerprint_sha256_b64)
+
+    def test_signature_invalide(self):
         # Tester validation signature
+        message_signe = self.contexte.generateur_transactions.preparer_enveloppe(
+            {'test': True}, domaine='test', ajouter_certificats=True)
         try:
             message_signe['en-tete']['allo'] = True
-            validateur.verifier(message_signe, utiliser_date_message=True)
+            self.validateur.verifier(message_signe, utiliser_date_message=True)
         except InvalidSignature:
             self.__logger.debug("Verification signature message - OK, flagge comme invalide")
         else:
             self.__logger.error("ERREUR - Verification signature message, flagge comme valide mais est invalide")
 
+    def test_hachage_invalide(self):
+        message_signe = self.contexte.generateur_transactions.preparer_enveloppe(
+            {'test': True}, domaine='test', ajouter_certificats=True)
         try:
             message_signe['allo'] = True
-            validateur.verifier(message_signe, utiliser_date_message=True)
+            self.validateur.verifier(message_signe, utiliser_date_message=True)
         except HachageInvalide:
             self.__logger.debug("Verification hachage message - OK, flagge comme invalide")
         else:
@@ -66,7 +71,14 @@ def main():
 
     test = ValiderMessage()
     test.initialiser_contexte()
+
+    test.test_valider_message_chargercertificat()
     test.test_valider_message()
+    test.test_signature_invalide()
+    test.test_hachage_invalide()
+
+    # Verifier le cache de certificat
+    test.test_valider_message_chargercertificat()
 
 
 if __name__ == '__main__':
