7486e0415730b7bfd96a858f482456ea6c0f35b6
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-02-18 16:27:37 +0000

    Modification IdmgUtil pour supporter multihash, longueur variable

--- millegrilles/util/IdmgUtil.py
+++ millegrilles/util/IdmgUtil.py
@@ -2,11 +2,16 @@
 import base58
 import math
 import struct
-import binascii
+import multibase
+import multihash
 
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
+from multihash.constants import HASH_CODES
+from typing import Union
+
+from millegrilles.util.Hachage import map_code_to_hashes
 
 
 class IdmgUtil:
@@ -16,45 +21,86 @@ class IdmgUtil:
 
     # Version courante de IDMG
     VERSION_ACTIVE = 1
-    VERSION_PACK = {1: '=B28sI'}
+    VERSION_PACK = {1: {'header': '=BI'}}
+
+    ENCODING = 'base58btc'
+    HASH_FUNCTION = 'sha2-256'
 
     def __init__(self):
         pass
 
     def encoder_idmg(self, certificat_pem: str, version=VERSION_ACTIVE):
-        cert_x509 = x509.load_pem_x509_certificate(certificat_pem.encode('utf-8'), default_backend())
-        return self.encoder_idmg_cert(cert_x509, version)
+        return encoder_idmg_cert(certificat_pem, version)
 
     def encoder_idmg_cert(self, cert_x509: x509, version=VERSION_ACTIVE):
-        date_exp = cert_x509.not_valid_after
-        date_exp_int = int(math.ceil(float(date_exp.timestamp()) / 1000.0))
-        valeur = cert_x509.fingerprint(hashes.SHA512_224())
-
-        valeur_combinee = struct.pack(IdmgUtil.VERSION_PACK[version], version, valeur, date_exp_int)
-        valeur_base58 = base58.b58encode(valeur_combinee).decode('utf-8')
-
-        return valeur_base58
+        return encoder_idmg_cert(cert_x509, version)
 
     def verifier_idmg(self, idmg: str, certificat_pem: str):
-        """
-        Verifie la correspondance du idmg avec un certificat
-        :param idmg: IDMG a verifier
-        :param certificat_pem: Certificat qui devrait correspondre au IDMG
-        :return:
-        :raises: IdmgInvalide si le Idmg ne correspond pas au certificat
-        """
-        # Extraire la version
-        valeur = base58.b58decode(idmg)
-        version = valeur[0]
+        return verifier_idmg(idmg, certificat_pem)
 
-        # Generer le IDMG pour cette version
-        try:
-            idmg_calcule = self.encoder_idmg(certificat_pem, version)
-        except KeyError:
-            raise IdmgInvalide("IDMG fourni en parametre est invalide pour version %d" % version)
 
-        if idmg_calcule != idmg:
-            raise IdmgInvalide("IDMG ne correspond pas au certificat")
+def encoder_idmg(certificat_pem: str, version=IdmgUtil.VERSION_ACTIVE, hashing_code: Union[int, str] = 'sha2-256'):
+    cert_x509 = x509.load_pem_x509_certificate(certificat_pem.encode('utf-8'), default_backend())
+    return encoder_idmg_cert(cert_x509, version, hashing_code)
+
+
+def encoder_idmg_cert(cert_x509: x509, version=IdmgUtil.VERSION_ACTIVE, hashing_code: Union[int, str] = 'sha2-256'):
+    if isinstance(hashing_code, str):
+        hashing_code = HASH_CODES[hashing_code]
+    hashing_function = map_code_to_hashes(hashing_code)
+    digest_fingerprint = cert_x509.fingerprint(hashing_function)
+
+    # Encoder hachage dans un multihash
+    mh = multihash.encode(digest_fingerprint, hashing_code)
+
+    date_exp = cert_x509.not_valid_after
+    date_exp_int = int(math.ceil(float(date_exp.timestamp()) / 1000.0))
+
+    version_info = IdmgUtil.VERSION_PACK[version]
+    header_struct = version_info['header']
+
+    valeur_combinee = struct.pack(header_struct, version, date_exp_int)
+    valeur_combinee = valeur_combinee + mh
+
+    mb = multibase.encode(IdmgUtil.ENCODING, valeur_combinee)
+
+    return mb.decode('utf-8')
+
+
+def verifier_idmg(idmg: str, certificat_pem: str):
+    """
+    Verifie la correspondance du idmg avec un certificat
+    :param idmg: IDMG a verifier
+    :param certificat_pem: Certificat qui devrait correspondre au IDMG
+    :return:
+    :raises: IdmgInvalide si le Idmg ne correspond pas au certificat
+    """
+    # Extraire la version
+    # valeur = base58.b58decode(idmg)
+    valeur = multibase.decode(idmg)
+    version = int(valeur[0])
+
+    version_info = IdmgUtil.VERSION_PACK[version]
+    header_struct = version_info['header']
+
+    header_size = struct.Struct(header_struct).size
+    (version, date_exp_int_recu) = struct.unpack(header_struct, valeur[0:header_size])
+    mh_bytes = valeur[header_size:]
+
+    mh = multihash.decode(mh_bytes)
+    hashing_code = mh.code
+    hashing_function = map_code_to_hashes(hashing_code)
+    digest_recu = mh.digest
+
+    cert_x509 = x509.load_pem_x509_certificate(certificat_pem.encode('utf-8'), default_backend())
+    digest_fingerprint_calcule = cert_x509.fingerprint(hashing_function)
+    if digest_recu != digest_fingerprint_calcule:
+        raise IdmgInvalide("IDMG ne correspond pas au certificat")
+
+    date_exp = cert_x509.not_valid_after
+    date_exp_int = int(math.ceil(float(date_exp.timestamp()) / 1000.0))
+    if date_exp_int_recu != date_exp_int:
+        raise IdmgInvalide("IDMG fourni en parametre est invalide - date expiration mismatch")
 
 
 class IdmgInvalide(BaseException):
---
+++ unit/util/IdmgUtilTest.py
@@ -0,0 +1,72 @@
+import logging
+
+from unittest import TestCase
+
+from millegrilles.util.IdmgUtil import IdmgInvalide, encoder_idmg, verifier_idmg
+
+
+# Setup logging
+logging.basicConfig()
+logger = logging.getLogger('__main__')
+logger.setLevel(logging.DEBUG)
+logging.getLogger('millegrilles').setLevel(logging.DEBUG)
+
+
+class IdmgUtilTest(TestCase):
+
+    SAMPLE_IDMG_1 = "zhNmDkHqvrHpk15RninDob9WJApPfBtVXJWjty8x2buXcdttWg4PN"
+    SAMPLE_IDMG_1_BLAKE2s = "zEZaD2K7EdL9LBngY1V28AaUzdHz9exWt4HpBBLZ8VZqWm2TPZsqWQjn"
+    SAMPLE_IDMG_1_BLAKE2b = "z52fL6uhxXDMFNESkcGKe1vJGUE1TLSpmXTzdBvDj6GxE8bPi2pZyHMXi5K6pcpFMmmGeGb5sJifpdhvrZwjM2pNHFs7R7fq8rv5"
+    SAMPLE_CERT_1 = """
+-----BEGIN CERTIFICATE-----
+MIIDJzCCAg+gAwIBAgIJh4hTAQFkKTIAMA0GCSqGSIb3DQEBDQUAMCcxDzANBgNV
+BAMTBlJhY2luZTEUMBIGA1UEChMLTWlsbGVHcmlsbGUwHhcNMjAxMDE1MTczNjQx
+WhcNMjExMDE1MTczNjQxWjAnMQ8wDQYDVQQDEwZSYWNpbmUxFDASBgNVBAoTC01p
+bGxlR3JpbGxlMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoEQE8xzF
+4BeTqnui0ri3F8wKGmF75xLKBUWklpc/FSnyIb6R/IfjoxT+tgI9Gr38lA9ITvdB
+ykNAeS4HbKv4K7g+vIWJixGWXBspd0Fs7FKMwZgN/c1wpBZw4rPjujk8u385/Aiu
+6WkCK0+QIPl5bmKWLIzs/wpcWt+g7vlFYSM7qKdvlxJ6LToqcZKrKVemPfokIJ+X
+QNe6vWTSVKqTCETe9ltPxnftI2eELuHpSyigYwkEIjkQPRbShpm/GdO7MJJwfo0i
+XJqAZabEAgJnCt1o0FNySRa8o5VThpiUDlbaAS77v0E/cgM8Q8+vbwZN3mAjzbn9
+xBYdSC2KMT5MFQIDAQABo1YwVDASBgNVHRMBAf8ECDAGAQH/AgEFMB0GA1UdDgQW
+BBQuZbJB1pbMNRIsl6wwUbkW+wIXVzAfBgNVHSMEGDAWgBQuZbJB1pbMNRIsl6ww
+UbkW+wIXVzANBgkqhkiG9w0BAQ0FAAOCAQEAOML4p+SwPU+VeTikYrH4tPQsTXnC
+Dt4VqI71MsTD4zOdKUN+voRaKQWO0RE3zcTfIcY784cDxvSrzpDWIkQ1OkAu9VvR
+MX1f9dlX3J7jEywnpHnEZ6uphew0PIApumXVsumGsztw+X8RAL8tX9a4V/xSzHwM
+Gls59U8FYZbvfIeo+IYxjbiK2tY44qU76tETdhJkUqbYwZKLveRv8UIjmaFAoybA
+CbpFuvHsuGMpL1Eg+nqDyn7z4GjAsjxu5UrCTlzXkUXyvGUcZ87zWFJo7ftG4EyM
+1D5hhfH0whmeLRxOs/BkYThHe3q+uis8K9R6qbdvXXmuw/nVUQU7QmL0mA==
+-----END CERTIFICATE-----
+    """
+
+    def test_generer_idmg(self):
+        idmg = encoder_idmg(IdmgUtilTest.SAMPLE_CERT_1)
+        logger.debug("IDMG calcule sur PEM : %s" % idmg)
+        self.assertEqual(idmg, IdmgUtilTest.SAMPLE_IDMG_1)
+
+    def test_generer_blake2s(self):
+        idmg = encoder_idmg(IdmgUtilTest.SAMPLE_CERT_1, hashing_code='blake2s-256')
+        logger.debug("IDMG calcule sur PEM : %s" % idmg)
+        self.assertEqual(idmg, IdmgUtilTest.SAMPLE_IDMG_1_BLAKE2s)
+
+    def test_generer_blake2b(self):
+        idmg = encoder_idmg(IdmgUtilTest.SAMPLE_CERT_1, hashing_code='blake2b-512')
+        logger.debug("IDMG calcule sur PEM : %s" % idmg)
+        self.assertEqual(idmg, IdmgUtilTest.SAMPLE_IDMG_1_BLAKE2b)
+
+    def test_verifier_idmg(self):
+        verifier_idmg(IdmgUtilTest.SAMPLE_IDMG_1, IdmgUtilTest.SAMPLE_CERT_1)
+
+    def test_verifier_idmg_blake2s(self):
+        verifier_idmg(IdmgUtilTest.SAMPLE_IDMG_1_BLAKE2s, IdmgUtilTest.SAMPLE_CERT_1)
+
+    def test_verifier_idmg_blake2b(self):
+        verifier_idmg(IdmgUtilTest.SAMPLE_IDMG_1_BLAKE2b, IdmgUtilTest.SAMPLE_CERT_1)
+
+    def test_generer_verifier_idmg(self):
+        idmg = encoder_idmg(IdmgUtilTest.SAMPLE_CERT_1)
+        logger.debug("IDMG calcule sur PEM : %s" % idmg)
+        verifier_idmg(idmg, IdmgUtilTest.SAMPLE_CERT_1)
+
+    def test_verifier_idmg_mismatch(self):
+        self.assertRaises(ValueError, verifier_idmg, 'IDMG dummy', IdmgUtilTest.SAMPLE_CERT_1)
