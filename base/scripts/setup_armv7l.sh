395eb02279fc5b2c77f6833bbed2556819bb9c71
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-07-21 17:00:58 +0000

    Nouvelle base pour 1.45

---
+++ base/scripts/setup_armv7l.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Fichier de setup pour container Docker (doit etre execute dans le container via Dockerfile)
+REQ_FILE=$BUILD_FOLDER/requirements.txt
+if [ ! -f $REQ_FILE ]; then
+    echo Fichier $REQ_FILE introuvable
+
+    echo Repertoire git: $GIT_FOLDER
+    echo Repertoire src: $SRC_FOLDER
+
+    ls -la $SRC_FOLDER
+    ls -la $GIT_FOLDER
+    exit 1
+fi
+
+echo "Installer rust pour compiler cryptography"
+apt update && apt install -y rustc
+
+echo "Installer dependances Python avec pip: fichier $REQ_FILE"
+# https_proxy=http://192.168.2.195:8000 pip3 install --no-cache-dir -r $REQ_FILE
+pip3 install --no-cache-dir -r $REQ_FILE
+
+# Fix pymongo, erreur cannot import abc (issue #305)
+pip3 uninstall -y bson
+pip3 uninstall -y pymongo
+pip3 install pymongo
+
+echo "Cleanup, retirer rustc"
+apt remove -y rustc
