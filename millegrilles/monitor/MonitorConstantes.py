13f0075363a5dcae75dcc06adc6306dffe57c617
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-10-13 18:21:04 +0000

    Ajout param nginx selon type noeud

--- millegrilles/monitor/MonitorConstantes.py
+++ millegrilles/monitor/MonitorConstantes.py
@@ -14,7 +14,7 @@ DICT_MODULES_PUBLICS = {
     ConstantesServiceMonitor.MODULE_ACME: {
         'nom': ConstantesServiceMonitor.MODULE_ACME
     },
-    ConstantesServiceMonitor.MODULE_NGINX: {
+    ConstantesServiceMonitor.MODULE_NGINX_PUBLIC: {
         'nom': ConstantesServiceMonitor.MODULE_NGINX_PUBLIC,
     },
 }
@@ -23,7 +23,7 @@ DICT_MODULES_PRIVES = {
     ConstantesServiceMonitor.MODULE_ACME: {
         'nom': ConstantesServiceMonitor.MODULE_ACME
     },
-    ConstantesServiceMonitor.MODULE_NGINX: {
+    ConstantesServiceMonitor.MODULE_NGINX_PUBLIC: {
         'nom': ConstantesServiceMonitor.MODULE_NGINX_PUBLIC,
     },
     # ConstantesServiceMonitor.MODULE_WEB_PROTEGE: {
